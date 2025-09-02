1b4c6b9892bfb1240da3197e7ab93ad44cdc7561
john-clemente7 <jonathanjc09@gmail.com>
2025-09-02 05:16:34 +0000

    super

---
+++ teste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+    int main(){
+        int idade = 26;
+        char nome[15] = 'Jonathan';
+
+        printf("A idade do %s é: %d \n, nome, idade");
+
+        return 0;
+
+
+
+    }
